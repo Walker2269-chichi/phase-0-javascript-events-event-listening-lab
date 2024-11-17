704d7914342f0a8d21cd17d0b2efa893c6475317
Walker2269-chichi <langatjeremiah22@gmail.com>
2024-11-17 19:31:06 +0300

    Javascript Event Listener

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    // Get the element with id 'button'
+  const button = document.getElementById('button');
+  
+  // Add an event listener to the button
+  button.addEventListener('click', () => {
+    console.log('Button clicked!');
+  });
 }
+
